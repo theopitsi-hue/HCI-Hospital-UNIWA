8d846094db9ffb08ef8185ace803fe7e83f0428d
johnkor09 <johnkor09@gmail.com>
2026-04-02 16:25:33 +0300

    basic settings panel

---
+++ Assets/Core/Graphics/UI/Scripts/OptionsManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsManager : MonoBehaviour
+{
+    public GameObject OptionsPanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        OptionsPanel.SetActive(false);
+        Time.timeScale = 1f; //συνεχιζει τον χρονο
+        isPaused = false;
+        
+    }
+
+    void Pause()
+    {
+        OptionsPanel.SetActive(true); 
+        Time.timeScale = 0f; //σταματαει τον χρονο
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
